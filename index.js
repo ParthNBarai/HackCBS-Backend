361bff367fd1e125e1c69dae713ef4a44a3d9572
Parth Barai <parthnbarai1234@gmail.com>
2023-11-04 16:04:58 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv/config')
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+const ConnectionDB = require("./database");
+// const multer = require('./Middlewares/multer')
+app.use(express.json());
+ConnectionDB();
+//Comments
+app.use(cors())
+// app.use("/api/image", multer.router)
+
+
+
+
+app.listen(port, () => console.log(`Server up and running...at ${port}`))
